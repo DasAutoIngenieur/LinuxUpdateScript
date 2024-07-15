0463f2b0f5d8623cef1575485908d14bbf215228
Jack Austin Zyler <73632160+DasAutoIngenieur@users.noreply.github.com>
2024-07-15 21:54:44 +0630

    Added Upgrade

--- updateupgrade.sh
+++ updateupgrade.sh
@@ -1,4 +1,6 @@
 if ping -c 1 google.com &> /dev/null; then
     sudo apt update && sudo apt-get update
+    sudo apt upgrade -y && sudo apt-get upgrade -y
 else
-    echo "No Network Connection, Check Network interfaces" 
+    echo "No Network Connection, Check Network interfaces"
+fi
